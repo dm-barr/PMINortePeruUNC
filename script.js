55ad589e8cc405ca229a3e8e85eaf890b88db877
Vonrabbid <vonrabbid@gmail.com>
2025-11-11 17:50:33 -0500

    Conexión GoogleSheets

--- script.js
+++ script.js
@@ -83,7 +83,10 @@ document.addEventListener("DOMContentLoaded", function () {
         overlay.classList.add("hidden");
     });
 
-    // ----- FORMULARIO DE VOLUNTARIADO EVENTOS -----
+    // ----- URL DEL SCRIPT DE GOOGLE FORMS -----
+    const scriptURL_Eventos = 'https://script.google.com/macros/s/AKfycbw8zBd1Lt0MCVJ3RIXjTUe3Ekxys_NTj8avfu9I2xIiWPoxL0qyL4VOZ_5-2sY16EhuHw/exec';
+
+    // ----- FORMULARIO DE EVENTOS -----
     const btnInscribirse = document.getElementById("btnInscribirse");
     const formModal = document.getElementById("formModal");
     const btnCerrarForm = document.getElementById("btnCerrarForm");
@@ -108,9 +111,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
         form.addEventListener("submit", (e) => {
             e.preventDefault();
-            alert("¡Gracias por inscribirte! Pronto nos pondremos en contacto contigo.");
-            form.reset();
-            formModal.classList.add("oculto");
+            const submitButton = form.querySelector('button[type="submit"]');
+            submitButton.disabled = true;
+            fetch(scriptURL_Eventos, { method: 'POST', body: new FormData(form) })
+                .finally(() => {
+                    form.reset();
+                    formModal.classList.add("oculto");
+                    submitButton.disabled = false;
+                });
         });
     }
 
